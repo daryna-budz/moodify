b191e2fdc9f9447534e985d4b5282749b753a4ae
daryna-budz <darynabudz@gmail.com>
2026-05-09 15:07:18 -0400

    feat: activity panel add + minor ui improvements

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import { Sun, Moon, Zap, Cloud, Flame, PartyPopper} from 'lucide-react';
+import { Sun, Moon, Zap, Cloud, Flame, PartyPopper, Dumbbell, PersonStanding, LaptopMinimal, BedDouble, CarFront} from 'lucide-react';
 import { useState } from "react";
 
 export default function Sidebar(){
     const [selectedMood, setSelectedMood] = useState('')
-
+    const [selectedActivity, setSelectedActivity] = useState('')
 
     return (
-        <main>
-            <section className='px-10 mt-5'>
+        <main className="md:flex">
+            <section className='px-10 md:border-r md:border-t md:border-gray-300 md:pr-15 md:pt-5'>
                 <h2 className='text-gray-500 font-semibold text-md uppercase text-center md:text-left'>Mood</h2>
                 <div className="grid grid-cols-2 grid-rows-3 gap-4 max-w-sm md:max-w-xs mx-auto md:mx-0 mt-3">
                         <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "happy" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("happy")}>
@@ -36,6 +36,30 @@ export default function Sidebar(){
                             <PartyPopper size={25} color="currentColor"/>
                             <p>Party</p>
                         </div>
+                </div>
+
+                <h2 className='text-gray-500 font-semibold text-md uppercase text-center md:text-left mt-5'>Activity</h2>
+                <div className='flex flex-col justify-center gap-4 max-w-sm mx-auto w-full mt-3 md:mx-0 md:max-w-xs'>
+                    <div className={`flex gap-2  border-2 border-gray-500 rounded-3xl p-3 cursor-pointer ${selectedActivity === "yoga" ? "text-green-600 border-green-600 bg-green-100" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedActivity("yoga")}>
+                        <PersonStanding size={25} color="currentColor"/>
+                        <p>Morning Yoga</p>
+                    </div>
+                    <div className={`flex gap-2  border-2 border-gray-500 rounded-3xl p-3 cursor-pointer ${selectedActivity === "work" ? "text-green-600 border-green-600 bg-green-100" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedActivity("work")}>
+                        <LaptopMinimal size={25} color="currentColor"/>
+                        <p>Work / Study</p>
+                    </div>
+                    <div className={`flex gap-2  border-2 border-gray-500 rounded-3xl p-3 cursor-pointer ${selectedActivity === "sleep" ? "text-green-600 border-green-600 bg-green-100" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedActivity("sleep")}>
+                        <BedDouble size={25} color="currentColor"/>
+                        <p>Sleep</p>
+                    </div>
+                    <div className={`flex gap-2  border-2 border-gray-500 rounded-3xl p-3 cursor-pointer ${selectedActivity === "workout" ? "text-green-600 border-green-600 bg-green-100" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedActivity("workout")}>
+                        <Dumbbell size={25} color="currentColor"/>
+                        <p>Workout</p>
+                    </div>
+                    <div className={`flex gap-2  border-2 border-gray-500 rounded-3xl p-3 cursor-pointer ${selectedActivity === "driving" ? "text-green-600 border-green-600 bg-green-100" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedActivity("driving")}>
+                        <CarFront size={25} color="currentColor"/>
+                        <p>Driving</p>
+                    </div>
                 </div>
 
             </section>
