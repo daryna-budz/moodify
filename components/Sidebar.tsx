3e86f938b7d2992eab23d6a2d2f7b2c9edc8b694
daryna-budz <darynabudz@gmail.com>
2026-05-09 14:19:04 -0400

    fix: renamed mood state

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Sun, Moon, Zap, Cloud, Flame, PartyPopper} from 'lucide-react';
 import { useState } from "react";
 
 export default function Sidebar(){
-    const [selected, setSelected] = useState('')
+    const [selectedMood, setSelectedMood] = useState('')
 
 
     return (
@@ -12,31 +12,32 @@ export default function Sidebar(){
             <section className='px-10 mt-5'>
                 <h2 className='text-gray-500 font-semibold text-md uppercase text-center md:text-left'>Mood</h2>
                 <div className="grid grid-cols-2 grid-rows-3 gap-4 max-w-sm md:max-w-xs mx-auto md:mx-0 mt-3">
-                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selected === "happy" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelected("happy")}>
+                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "happy" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("happy")}>
                             <Sun size={25} color="currentColor"/>
                             <p>Happy</p>
                         </div>
-                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selected === "calm" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelected("calm")}>
+                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "calm" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("calm")}>
                             <Moon size={25} color="currentColor"/>
                             <p>Calm</p>
                         </div>
-                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selected === "pumped" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelected("pumped")}>
+                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "pumped" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("pumped")}>
                             <Zap size={25} color="currentColor"/>
                             <p>Pumped</p>
                         </div>
-                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selected === "melancholic" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelected("melancholic")}>
+                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "melancholic" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("melancholic")}>
                             <Cloud size={25} color="currentColor"/>
                             <p>Melancholic</p>
                         </div>
-                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selected === "focused" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelected("focused")}>
+                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "focused" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("focused")}>
                             <Flame size={25} color="currentColor"/>
                             <p>Focused</p>
                         </div>
-                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selected === "party" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelected("party")}>
+                        <div className={`flex flex-col items-center gap-1 border-2 border-gray-500 rounded-md p-5 cursor-pointer ${selectedMood === "party" ? "text-pink-300 border-pink-300 bg-pink-50" : "text-gray-500 border-gray-500"}`} onClick={()=> setSelectedMood("party")}>
                             <PartyPopper size={25} color="currentColor"/>
                             <p>Party</p>
                         </div>
                 </div>
+
             </section>
         </main>
     )
