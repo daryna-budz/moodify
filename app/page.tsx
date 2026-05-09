79327db39a9bf7a28b6f3be4c6c65c593e21cc5b
daryna-budz <darynabudz@gmail.com>
2026-05-08 22:11:35 -0400

    feat: Navbar add

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
+
 
 export default function Home() {
   return (
-    <div >
-      <p>Hello Moodify!</p>
-    </div>
+    <Navbar />
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { Sparkles } from 'lucide-react';
+
+export default function Navbar(){
+    return (
+        <header className="px-8 py-10 md:px-10">
+          <nav className="flex items-center justify-between">
+            <Link href="/" className="flex items-center gap-3">
+                  <Sparkles size={40} color="#1d293d"/>
+                  <h1 className="text-2xl md:text-4xl font-bold text-slate-800">Moodify</h1>
+            </Link>
+            <div>
+                 <button className="text-xl md:text-2xl border-3 text-slate-800 rounded-4xl p-3 cursor-pointer hover:text-white hover:bg-slate-800 transition-all duration-300">Connect Spotify</button>
+            </div>
+          </nav>
+        </header>
+      );
+}
