577bc552759343c5c35f60e1eb93442fb8b1ff75
daryna-budz <darynabudz@gmail.com>
2026-05-16 23:26:27 -0400

    fix: logged in user display

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { Sparkles } from 'lucide-react';
-import { signIn } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 
 
 export default function Navbar(){
-
+    const { data: session } = useSession()
 
     return (
         <header className="px-8 py-10 md:px-10">
@@ -16,7 +16,21 @@ export default function Navbar(){
                   <h1 className="text-2xl md:text-4xl font-bold text-slate-800">Moodify</h1>
             </div>
             <div>
-                 <button onClick={() => signIn("spotify")} className="text-xl md:text-2xl font-semibold border-3 text-slate-800 rounded-4xl p-3 cursor-pointer hover:text-white hover:bg-slate-800 transition-all duration-300">Connect Spotify</button>
+              {!session ? (
+                <button onClick={() => signIn("spotify")} className="text-xl md:text-2xl font-semibold border-3 text-slate-800 rounded-4xl p-3 cursor-pointer hover:text-white hover:bg-slate-800 transition-all duration-300">Connect Spotify</button>
+              ) : (
+                <div className="flex items-center gap-4">
+            
+                  {session.user?.image && (
+                    <img
+                      src={session.user.image}
+                      className="w-10 h-10 rounded-full"
+                    />
+                  )}
+                  <span className="font-semibold">{session.user?.name}</span>
+                  <button onClick={() => signOut()}> Logout </button>
+                </div>
+              )}
             </div>
           </nav>
         </header>
